cde24b1bf9717be5ae0c98024fe8ef96c4014852
Maxim Semikov <maxim.semikov87@gmail.com>
2024-10-17 22:03:31 +0500

    feat: 08-snapshot-testing

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,30 @@
-// Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
+  const input = ['a', 'b', 'c'];
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const expectedLinkedList = {
+      value: 'a',
+      next: {
+        value: 'b',
+        next: {
+          value: 'c',
+          next: {
+            value: null,
+            next: null,
+          },
+        },
+      },
+    };
+
+    const result = generateLinkedList(input);
+    expect(result).toStrictEqual(expectedLinkedList);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const resultList = generateLinkedList(input);
+    expect(resultList).toMatchSnapshot();
   });
 });
